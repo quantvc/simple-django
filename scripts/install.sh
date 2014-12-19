fe6fc5642835002e2e2488b177501ff01ad53c72
王伟晨 <nooper@sfdeMacBook-Air.local>
2014-12-19 11:39:51 +0800

    add the install bash.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+
+python manage.py syncdb --settings=quantvc.settings.dev --noinput
+python manage.py migrate --settings=quantvc.settings.dev
+python manage.py collectstatic --settings=quantvc.settings.dev --noinput
